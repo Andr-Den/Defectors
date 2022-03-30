4b640f67ff3079d1692fe1876d170f104b5a2ed4
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-30 22:21:20 +0500

    add delete function in api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,39 +1,43 @@
 export const BASE_URL = 'http://localhost:3000';
 
-export const getCompanies = () => {
-  return fetch(`${BASE_URL}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  .then(checkResponse)
-  .then((res) => {
-    return res;
-  })
-};
-
-export const addCompany = (name, website, date) => {
-  return fetch(`${BASE_URL}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name, website, date
-    }
-    )
-  })
-  .then(checkResponse)
-  .then((res) => {
-    return res;
-  })
-};
-
 const checkResponse = (res) => {
   if (res.ok) {
     return res.json();
   }
 
   return Promise.reject(`Ошибка: ${res.status}`);
-}
+};
+
+export const getCompanies = () => fetch(`${BASE_URL}`, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then(checkResponse)
+  .then((res) => res);
+
+export const addCompany = (name, website, date) => fetch(`${BASE_URL}`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    name, website, date,
+  }),
+})
+  .then(checkResponse)
+  .then((res) => res);
+
+  export const deleteCompany = (data) => {
+    return fetch(`${BASE_URL}/` + data._id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(checkResponse)
+    .then((res) => {
+      return res;
+    })
+  };
