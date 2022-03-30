653510958e7912bdcc38172a92d81e7120d44054
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-30 22:24:47 +0500

    add delete button

--- frontend/src/components/DefectorCard/DefectorCard.js
+++ frontend/src/components/DefectorCard/DefectorCard.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function DefectorCard({name, website, date}) {
-return (
-  <li className="movie-card">
+function DefectorCard({ name, website, date }) {
+  return (
+    <li className="movie-card">
         <p className="">{`Название компании: ${name}`}</p>
         <p className="">{`Ссылка на новость: ${website}`}</p>
         <p className="">{`Дата: ${date}`}</p>
-  </li>
-)}
+        <button>Удалить</button>
+    </li>
+  );
+}
 
 export default DefectorCard;
