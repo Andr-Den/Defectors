db8b4a68b1f6d117df73139d787e1188e5037ccd
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-31 00:06:35 +0500

    refactor defectorsList

--- frontend/src/components/DefectorsList/DefectorsList.js
+++ frontend/src/components/DefectorsList/DefectorsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { Button, Table } from 'react-bootstrap';
 import DefectorCard from '../DefectorCard/DefectorCard';
 import { allCompanies, getCompanies } from '../../app/reducers/companiesSlice';
 
@@ -10,14 +11,25 @@ function DefectorsList() {
 
   return (
     <>
-      <button onClick={() => dispatch(getCompanies())}>
+      <Button onClick={() => dispatch(getCompanies())} className="mt-3">
         Список компаний
-      </button>
-      <ul className="">
-        {companies.map((company) => (
-          <DefectorCard company={company} key={company._id}/>
-        ))}
-      </ul>
+      </Button>
+      <Table striped bordered hover className="mt-3">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Имя</th>
+            <th>Ссылка на новость</th>
+            <th>Дата</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {companies.map((company, index) => (
+            <DefectorCard company={company} key={company._id} index={index}/>
+          ))}
+        </tbody>
+      </Table>
     </>
   );
 }
