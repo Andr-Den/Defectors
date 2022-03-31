b8d6ea1cfd651882beaa846dd383290c5b944776
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-31 17:59:56 +0500

    refactor AddPage modal

--- frontend/src/components/AddPage/AddPage.js
+++ frontend/src/components/AddPage/AddPage.js
@@ -7,17 +7,19 @@ function AddPage({setPageOpen}) {
   const dispatch = useDispatch();
 
   return (
-    <Modal.Dialog>
-      <Modal.Header >
-        <Modal.Title>Добавление компании</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <AddForm />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => dispatch(setPageOpen(false))}>Close</Button>
-      </Modal.Footer>
+    <div className="position-fixed d-flex w-100 h-100">
+      <Modal.Dialog className="position-relative">
+        <Modal.Header >
+          <Modal.Title>Добавление компании</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => dispatch(setPageOpen(false))}>Close</Button>
+        </Modal.Footer>
       </Modal.Dialog>
+    </div>
   )
 }
 
