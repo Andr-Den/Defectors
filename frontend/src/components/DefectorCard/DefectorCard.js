8558437011b10090ddc96680d9e496dd6e8f8c81
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-04-01 20:53:00 +0500

    remove unnecessary components

--- frontend/src/components/DefectorCard/DefectorCard.js
+++ frontend/src/components/DefectorCard/DefectorCard.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { deleteCompany } from '../../app/reducers/companiesSlice';
 import { Button } from 'react-bootstrap';
-import EditPage from '../EditPage/EditPage';
-
-import icon from '../../image/delete.svg'
-import edit from '../../image/edit.svg'
+import { useRouter } from 'next/router';
+import { deleteCompany } from '../../app/reducers/companiesSlice';
 
 function DefectorCard({ company, index }) {
-  const [editPageOpen, setEditPageOpen] = React.useState(false);
   const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    router.push(`/edit/${company._id}`)
+  }
+
   return (
     <>
-    {editPageOpen ? <EditPage setEditPageOpen={setEditPageOpen} company={company}/> : ''}
     <tr>
       <td>{index + 1}</td>
       <td>{company.name}</td>
       <td><a href={company.website} target='_blank' rel='noreferrer'>{company.website}</a></td>
       <td>{company.date}</td>
-      <td>
-        <Button variant="primary" onClick={() => dispatch(setEditPageOpen(true))}><img src={edit} alt="редактирование компании"/></Button>
-        <Button variant="danger" onClick={() => dispatch(deleteCompany(company))}><img src={icon} alt="удаление компании"/></Button>
+      <td className="d-flex justify-content-between">
+        <Button variant="primary" onClick={handleClick}>Редактировать</Button>
+        <Button variant="danger" onClick={() => dispatch(deleteCompany(company))}>Удалить</Button>
       </td>
     </tr>
   </>
