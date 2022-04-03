81029183957d14358e8fca076266f7d009d16a4c
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-04-03 18:26:51 +0500

    add styles for component defectorCard

--- frontend/src/components/DefectorCard/DefectorCard.js
+++ frontend/src/components/DefectorCard/DefectorCard.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 
+import Icon from "../../images/delete.svg"
+import Edit from "../../images/edit.svg"
+import View from "../../images/view.svg"
+
 function DefectorCard({ company, index }) {
   const router = useRouter();
 
@@ -21,15 +25,15 @@ function DefectorCard({ company, index }) {
 
   return (
     <>
-    <tr>
-      <td>{index + 1}</td>
-      <td>{company.name}</td>
-      <td><a href={company.website} target='_blank' rel='noreferrer'>{company.website}</a></td>
-      <td>{company.date}</td>
+    <tr className="c">
+      <td className="">{index + 1}</td>
+      <td className="">{company.name}</td>
+      <td className=""><a href={company.website} target='_blank' rel='noreferrer'>{company.website}</a></td>
+      <td className="">{company.date}</td>
       <td className="d-flex justify-content-between">
-        <Button variant="danger" onClick={handleViewClick}>Просмотр</Button>
-        <Button variant="primary" onClick={handleClick}>Редактировать</Button>
-        <Button variant="danger" onClick={handleDeleteClick}>Удалить</Button>
+        <Button variant="dark" onClick={handleViewClick}><View /></Button>
+        <Button variant="primary" onClick={handleClick}><Edit /></Button>
+        <Button variant="danger" onClick={handleDeleteClick}><Icon /></Button>
       </td>
     </tr>
   </>
