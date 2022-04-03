3a96466aef213203d5ef8e5fdea3fca9025cf535
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-04-03 18:25:32 +0500

    add component footer

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="w-75 m-auto mt-5 d-flex gap-3">
+      <span>&copy; 2022 <a href="https://github.com/Andr-Den" target="_blank" rel="noreferrer" > Андрей Денисов</a></span>
+  </footer>
+  )
+}
+
+export default Footer;
