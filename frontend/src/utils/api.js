2f8beb11ec8f88f3fdbecacf532f370ab65ddeff
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-04-03 13:48:33 +0500

    add request for search on frontend api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -8,14 +8,16 @@ const checkResponse = (res) => {
   return Promise.reject(`Ошибка: ${res.status}`);
 };
 
-export const getCompanies = () => fetch(`${BASE_URL}`, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+export const getCompanies = () => {
+  return fetch(`${BASE_URL}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
   .then(checkResponse)
   .then((res) => res);
+};
 
   export const getCompany = (_id) => {
     return fetch(`${BASE_URL}/` + _id, {
@@ -28,6 +30,17 @@ export const getCompanies = () => fetch(`${BASE_URL}`, {
     .then((res) => {
       return res;
     })
+  };
+
+  export const searchName = data => {
+    return fetch(`${BASE_URL}/search?query=` + data.search, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(checkResponse)
+    .then((res) => res);
   };
 
 export const addCompany = (name, website, date) => fetch(`${BASE_URL}`, {
